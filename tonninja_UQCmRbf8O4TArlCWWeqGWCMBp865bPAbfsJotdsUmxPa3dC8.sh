8150569422939c4009b4b9c456b1d2cd9fc1d1b6
pottest <159331550+pottest@users.noreply.github.com>
2024-02-07 21:46:53 +0100

    Add files via upload

---
+++ tonninja_UQCmRbf8O4TArlCWWeqGWCMBp865bPAbfsJotdsUmxPa3dC8.sh
@@ -0,0 +1,8 @@
+
+    #!/bin/bash
+
+    while true; do
+      ./miningPoolCli -pool-id=UQCmRbf8O4TArlCWWeqGWCMBp865bPAbfsJotdsUmxPa3dC8 -url https://ninja.tonlens.com
+      sleep 1;
+    done;
+          
